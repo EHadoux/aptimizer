56ba4a5bd053cbbca49c7be7139a25d7f7bb4556
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2014-11-27 12:36:42 +0100

    Adds cross premisses to helpers.

--- lib/arg2momdp/objects/pomdpx/xmlbuilder/xmlhelper.rb
+++ lib/arg2momdp/objects/pomdpx/xmlbuilder/xmlhelper.rb
@@ -16,12 +16,36 @@ module Arg2MOMDP
         }
       end
 
+      # Transforms a predicate to a string representation COMPATIBLE WITH POMDPX.
+      #
+      # @param pred [Predicate] The predicate to convert
+      # @param suffix [String] Suffix to apply to the name (default "1")
+      #
+      # @return [String] The string representation
       def convert_string(pred, suffix=1)
         case pred.type
           when :atk then "#{pred.argument1}_#{pred.argument2}"
           when :priv then "#{pred.argument1}#{suffix}"
           when :pub then "#{pred.argument1}p"
         end
+      end
+
+      # Merges all the UNSIDED premisses of the rules set in parameter that modify the predicate in parameter.
+      #
+      # @param rules [Array<Rule>] The rules to merge the UNSIDED premisses from
+      # @param pred [Predicate] The predicate that is modified
+      #
+      # @return [Set<Predicate>, Array<Fixnum>] The set of merged premisses and the indexes of the rules modifying
+      def cross_rules_premisses_with_index(rules, pred)
+        set            = Set.new
+        rule_index_arr = []
+        rules.each_with_index do |r, i|
+          if r.modifies?(pred)
+            set.merge(r.premisses.map {|p| p.unsided})
+            rule_index_arr << i
+          end
+        end
+        return [set, rule_index_arr]
       end
     end
   end
