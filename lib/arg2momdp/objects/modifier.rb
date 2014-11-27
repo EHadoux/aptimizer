acca29eda48b7d27c232c9b90e5447de297cf8dd
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2014-11-27 12:02:03 +0100

    Adds compatible? to modifier and doc.

--- lib/arg2momdp/objects/modifier.rb
+++ lib/arg2momdp/objects/modifier.rb
@@ -2,6 +2,13 @@ module Arg2MOMDP
   class Modifier
     attr_reader :type, :scope, :predicate
 
+    # Constructs a modifier on a [Predicate].
+    #
+    # @param type [:add, :rem] Specifies if this modifier adds or removes a predicate
+    # @param scope [:priv, :pub] Specifies if this modifier changes the private state or the public space
+    # @param predicate [Predicate] The predicate to be added or removed (only positive predicate can be modified)
+    # @raise [Error] if type or scope is invalid, the predicate is negative,
+    #   the scope and the type of the predicate are not compatible
     def initialize(type, scope, predicate)
       @type      = type
       @scope     = scope
@@ -9,6 +16,7 @@ module Arg2MOMDP
 
       raise "Unknown modifier type: #{type}" unless [:add, :rem].include? type
       raise "Unknown modifier scope: #{scope}" unless [:priv, :pub].include? scope
+      raise "Cannot modify a negative predicate" unless @predicate.positive
 
       case @predicate.type
       when :atk then raise "No private modifiers on attacks" if @scope == :priv
@@ -17,8 +25,31 @@ module Arg2MOMDP
       end
     end
 
+    # Returns a readable version of the modifier.
+    #
+    # return [String] A String representation of the modifier
     def to_s
       "#{"." if @scope == :priv}#{(@type == :add) ? "+" : "-"}#{@predicate}"
+    end
+
+    # Returns whether the modifier is compatible with an instance of predicate.
+    # If the instance is "*", all modifiers are compatible.
+    # If the instance is true, the modifier type can only be :rem as it cannot add an already present predicate.
+    # It is the opposite for false.
+    #
+    # @param instance ["*", true, false] The instance to test the modifier on
+    #
+    # @return [Boolean] True if the modifier is compatible, false otherwise
+    #
+    # @raise [Error] if the instance is not "*", true or false
+    def compatible?(instance)
+      if instance.is_a?(String)
+        return true if instance == "*"
+      elsif !!instance == instance
+        return (@type == :add && !instance) || (@type == :rem && instance)
+      else
+        raise "Can only test with *, true or false"
+      end
     end
   end
 end
--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -10,9 +10,29 @@ RSpec.describe Arg2MOMDP::Modifier do
     expect {Arg2MOMDP::Modifier.new(:unknown, nil, nil)}.to raise_error
   end
 
+  it "raises an error if the predicate is negative" do
+    p = double(:positive => false)
+    expect {Arg2MOMDP::Modifier.new(:add, :pub, p)}.to raise_error
+  end
+
+  it "checks the compatibility with an instance" do
+    pospred = double(:positive => true, :type => :pub)
+    mod = Arg2MOMDP::Modifier.new(:add, :pub, pospred)
+    expect {mod.compatible?(1)}.to raise_error
+    expect(mod.compatible?("")).to be_falsey
+    expect(mod.compatible?("*")).to be_truthy
+    expect(mod.compatible?(false)).to be_truthy
+    expect(mod.compatible?(true)).to be_falsey
+
+    mod = Arg2MOMDP::Modifier.new(:rem, :pub, pospred)
+    expect(mod.compatible?("*")).to be_truthy
+    expect(mod.compatible?(true)).to be_truthy
+    expect(mod.compatible?(false)).to be_falsey
+  end
+
   context "Public" do
     before(:each) do
-      @pred_pub = double(:type => :pub, :to_s => "a(a)")
+      @pred_pub = double(:type => :pub, :to_s => "a(a)", :positive => true)
     end
 
     it "raises an error if the scope is private and the predicate public" do
@@ -29,8 +49,8 @@ RSpec.describe Arg2MOMDP::Modifier do
 
   context "Private" do
     before(:each) do
-      @pred_priv = double(:type => :priv, :to_s => "h(a)")
-      @pred_atk  = double(:type => :atk, :to_s => "e(a, b)")
+      @pred_priv = double(:type => :priv, :to_s => "h(a)", :positive => true)
+      @pred_atk  = double(:type => :atk, :to_s => "e(a, b)", :positive => true)
     end
 
     it "raises an error if the scope is public and the predicate private" do
